08a599f39b4d18f7417835042a0bd09a615abee2
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-26 22:43:57 +0300

    11/16

--- src/index.js
+++ src/index.js
@@ -1,38 +1,30 @@
 module.exports = function count(s, pairs) {
-  pairs.sort((a, b) => a[0] - b[0]);
-  let N = calculateN(pairs);
 
+  let N = calculateN(pairs);
   let counterRes = 0;
-  if (N === Infinity) return 0;
-  for (let i = 1; i <= N; i++){
-    if (testNumber(s, i, pairs) === true){counterRes++};
+
+  if (N > 10000000) return 0;
+
+  if (s === '1' && s.length === 1){
+    counterRes = 1;
+    pairs.forEach(item => counterRes *= (item[0] - 1));
+    return counterRes;
   }
 
-  if (N.count > 0) counterRes = counterRes * N.count;
+  for (let i = 1; i <= N; i++){
+    if (testNumber(s, i, N) === true){counterRes++};
+  }
 
   return counterRes % 1000000007;
 }
 
 function calculateN(mas){
   let n = 1;
-  let count = 0;
 
   for (let i = 0; i < mas.length; i++){
-
-    // for (let j = 1; j <= mas[i][1]; j++){
-    //   n *= mas[i][0];
-    //   if (n / 1000000000 > 1){
-    //     count += Math.trunc(n / 1000000000);
-    //     n = n % 1000000000;
-    //   }
-    // }
-
     n *= Math.pow(mas[i][0], mas[i][1]);
-
   }
-  // return {
-  //         value: (count > 0) ? 1000000000 : n,
-  //         count: count};
+
   return n;
 }
 
@@ -47,26 +39,17 @@ function nod(n, m) {
   }
 }
 
-function testNumber (s, num, mas){
+function testNumber (s, num, N){
   for (let i = 0; i < s.length; i++){
 
-     if (s[i] === '0' && testAllMultipliers((num + i), mas) === false){
+     if (s[i] === '0' && nod(N, num + i) === 1){
        return false;
      }
 
-     if (s[i] === '1' && testAllMultipliers((num + i), mas) === true){
+     if (s[i] === '1' && nod(N, num + i) !== 1){
        return false;
      }
   }
 
   return true;
 }
-
-function testAllMultipliers(num, mas){
-
-  for (let i = 0; i < mas.length; i++){
-    if (nod(num, mas[i][0]) !== 1) return true;
-  }
-  return false;
-}
-
